0bd61b8f5cecddb40eb64cdcb30d6a0c0cf20fb0
Kunal Singh <kunal1998doon@gmail.com>
2019-10-28 15:56:00 +0530

    starting physics based 2d player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rg;
+    private bool isGrounded;
+    void Start()
+    {
+         rg = this.GetComponent<Rigidbody2D>();
+         isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        rg.velocity = new Vector2(Horizontal, 0.0f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
